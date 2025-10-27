a4c0b5f4fef92863c54bb19cb528ed5b9901db3d
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-10-27 20:55:19 +0100

    mandatory error  exercise solution 1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
-
 const age = 33;
+age = age + 1;
+
+// instead of const, we should use let because it means that the variable can change
+
+let age = 33;
 age = age + 1;
