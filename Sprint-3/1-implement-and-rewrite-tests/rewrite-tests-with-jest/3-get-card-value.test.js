1afbbbfdedb07e1cb379e1592473ce559ceb7408
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-11-20 14:28:24 +0100

    exercise 3 solution rewrite test

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -2,12 +2,25 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
+test("should return 11 for Aces", () => {
   const aceofSpades = getCardValue("A♠");
   expect(aceofSpades).toEqual(11);
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return a rank between (2-9) for any card !== A, J, Q, K", () => {
+  const fiveOfHearts = getCardValue("5♥");
+  expect(fiveOfHearts).toEqual(5);
+});
 // Case 3: Handle Face Cards (J, Q, K):
-// Case 4: Handle Ace (A):
+test("should return 10 for face cards (J, Q, K)", () => {
+  const jackOfClubs = getCardValue("J♣");
+  expect(jackOfClubs).toEqual(10);
+});
+// Case 4: Handle Ace (A): The first test i made works for this one too
+
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card strings", () => {
+  expect(() => getCardValue("Invalid")).toThrow(); 
+  expect(() => getCardValue("100♥")).toThrow();
+});
