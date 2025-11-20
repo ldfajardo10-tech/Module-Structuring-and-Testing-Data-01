d49e1b0b7cf3e2432df55097ef184d98964af83b
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-11-20 17:03:10 +0100

    repeat exercise solution 2-practice-tdd

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,16 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  const numCount = Math.floor(count);
+
+  if (numCount >= 1) {
+    return str.repeat(count);
+  }
+  else if (numCount === 0) {
+    return "";
+  }
+  else if (numCount < 0) {
+    return "invalid count";
+  }
 }
 
 module.exports = repeat;
+
--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -20,13 +20,29 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
-
+test("should not repeat the string", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
-
+test("should return an empty string when the count is 0", () => {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("should return an error message when the count is negative", () => {
+  const str = "hello";
+  const count = -3;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("invalid count");
+});
