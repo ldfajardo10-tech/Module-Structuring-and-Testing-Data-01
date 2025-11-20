806337a7f1c78e941ba7245df9735714be108294
ldfajardo10-tech <ld.fajardo10@gmail.com>
2025-11-20 16:01:29 +0100

    count exercise test and function solution

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let count = 0;
+  
+for (let i = 0; i < stringOfCharacters.length; i++) { // we create a for loop so it can find and count the characters
+  if (stringOfCharacters[i] === findCharacter) {
+    count++;
+  }
+}
+return count;
+
 }
 
 module.exports = countChar;
