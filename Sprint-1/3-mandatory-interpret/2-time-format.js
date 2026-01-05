efcfadf7a1a24a5cf4157d74c2f059493282b341
ldfajardo10-tech <ld.fajardo10@gmail.com>
2026-01-05 22:12:44 +0100

    exercise solution 2-time-format, mandatory interpret

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 2500000; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -11,15 +11,16 @@ console.log(result);
 
 // For the piece of code above, read the code and then answer the following questions
 
-// a) How many variable declarations are there in this program?
+// a) How many variable declarations are there in this program? - six variable declarations
 
-// b) How many function calls are there?
+// b) How many function calls are there? - Ten counting the log(result)
 
-// c) Using documentation, explain what the expression movieLength % 60 represents
+// c) Using documentation, explain what the expression movieLength % 60 represents - Represents how many seconds remain from the movie. how many times is 60 in 8784? = 24 so that would be the remainder 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
-// d) Interpret line 4, what does the expression assigned to totalMinutes mean?
 
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// d) Interpret line 4, what does the expression assigned to totalMinutes mean? - Represent how many minutes of the movie have pass
 
-// f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// e) What do you think the variable result represents? Can you think of a better name for this variable? - Represents the film time elapsed and a better name could be : const time elapsed = 
+
+// f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer - yes, because the format is in hours, could works with any positive number.
